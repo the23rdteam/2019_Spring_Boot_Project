18d762d63b10db8f6151f2f192e1e8997668419b
ImHoJeong <39ghwjd@naver.com>
2019-04-08 18:47:37 +0900

    Add Initial Project

---
+++ src/main/java/com/the23rdTeam/HomeWork/HomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.the23rdTeam.HomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkApplication.class, args);
+	}
+
+}
