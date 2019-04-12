74b0263bb6b64b43734b6a1e6ff81999b7591e76
ImHoJeong <39ghwjd@naver.com>
2019-04-12 22:27:12 +0900

    EncryptedSummonerApliClient 필드 선언

--- src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
+++ src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
@@ -1,7 +1,9 @@
 package com.the23rdTeam.HomeWork.service;
 
+import com.the23rdTeam.HomeWork.api.EncryptedSummonerIdApiClient;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LolEncryptedSummonerIdService {
+    private EncryptedSummonerIdApiClient encryptedSummonerIdApiClient;
 }
