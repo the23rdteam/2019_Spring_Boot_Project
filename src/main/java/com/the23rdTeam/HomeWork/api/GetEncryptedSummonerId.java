070c37e4fcd4c0551c258af40343eb54bbd1a9d8
ImHoJeong <39ghwjd@naver.com>
2019-04-12 21:55:19 +0900

    RestTemplate 추가(외부 웹 서버의 API 호출 목적)

--- src/main/java/com/the23rdTeam/HomeWork/api/GetEncryptedSummonerId.java
+++ src/main/java/com/the23rdTeam/HomeWork/api/GetEncryptedSummonerId.java
@@ -1,9 +1,12 @@
 package com.the23rdTeam.HomeWork.api;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class GetEncryptedSummonerId {
 
+    private RestTemplate restTemplate;
 
 }
