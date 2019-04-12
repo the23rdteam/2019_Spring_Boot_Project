2ea0f039fa818d26955e22262752fe6fcf594f05
ImHoJeong <39ghwjd@naver.com>
2019-04-12 22:32:28 +0900

    getEncryptedSummonerIdApiClient 메소드 정의

--- src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
+++ src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
@@ -4,8 +4,14 @@ import com.the23rdTeam.HomeWork.api.EncryptedSummonerIdApiClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LolEncryptedSummonerIdService {
     @Autowired
     private EncryptedSummonerIdApiClient encryptedSummonerIdApiClient;
+
+    public List<String> getEncryptedSummonerIdApiClient(){
+        return null;
+    }
 }
