bf27659d0d30bf6478877c4a16ad5ac6c1aa3af7
ImHoJeong <39ghwjd@naver.com>
2019-04-12 21:56:38 +0900

    @Autowired annotation 추가(Spring Container야 RestTemplate객체를 주입해줘!)

--- src/main/java/com/the23rdTeam/HomeWork/api/GetEncryptedSummonerId.java
+++ src/main/java/com/the23rdTeam/HomeWork/api/GetEncryptedSummonerId.java
@@ -1,5 +1,6 @@
 package com.the23rdTeam.HomeWork.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class GetEncryptedSummonerId {
 
+    @Autowired
     private RestTemplate restTemplate;
-
 }
+
