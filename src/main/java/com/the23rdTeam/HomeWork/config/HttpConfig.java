4a5f04ff22dd8316e3b2fe6284258a22e3207125
ImHoJeong <39ghwjd@naver.com>
2019-04-12 22:00:42 +0900

    RestTemplate 객체를 반환하는 메소드를 추가

--- src/main/java/com/the23rdTeam/HomeWork/config/HttpConfig.java
+++ src/main/java/com/the23rdTeam/HomeWork/config/HttpConfig.java
@@ -1,7 +1,11 @@
 package com.the23rdTeam.HomeWork.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class HttpConfig {
+    public RestTemplate createRestTemplate(){
+        return new RestTemplate();
+    }
 }
