9019a1de0bb5b87fb4ac9b19fc0cc727dca3a4ee
ImHoJeong <39ghwjd@naver.com>
2019-04-12 22:41:17 +0900

    @RequestMapping annotation을 controller 클래스에 추가 & 주석 설명 추가

--- src/main/java/com/the23rdTeam/HomeWork/controller/LolEncryptedSummonerIdController.java
+++ src/main/java/com/the23rdTeam/HomeWork/controller/LolEncryptedSummonerIdController.java
@@ -1,10 +1,16 @@
 package com.the23rdTeam.HomeWork.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ Controller 클래스에 @RequestMapping 추가
+ => controller 클래스에서 제공되는 API들의 기본 URI가
+ http://localhost:8080/weather-crawler와 같이 매핑됨.
+ */
 @RestController
+@RequestMapping("/lolEncryptedSummonerId")
 public class LolEncryptedSummonerIdController {
-
 
 }
