bc076dae02386c71bd7449657bdbb2599bcc395d
ImHoJeong <39ghwjd@naver.com>
2019-04-12 22:30:27 +0900

    @Autowired annotation 추가(encryptedSummonerIdApiClient 필드에 Spring Container가 객체를 주입(DI)목적)

--- src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
+++ src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
@@ -1,9 +1,11 @@
 package com.the23rdTeam.HomeWork.service;
 
 import com.the23rdTeam.HomeWork.api.EncryptedSummonerIdApiClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LolEncryptedSummonerIdService {
+    @Autowired
     private EncryptedSummonerIdApiClient encryptedSummonerIdApiClient;
 }
