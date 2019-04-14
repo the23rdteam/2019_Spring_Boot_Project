48eb25b9552a47ede86525d6c69328298f92a9d3
ImHoJeong <39ghwjd@naver.com>
2019-04-14 16:08:11 +0900

    ShowLeaguePositionApiClient 필드 선언 및 @Autowired 어노테이션 추가

--- src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
+++ src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
@@ -1,6 +1,7 @@
 package com.the23rdTeam.HomeWork.service;
 
 import com.the23rdTeam.HomeWork.api.EncryptedSummonerIdApiClient;
+import com.the23rdTeam.HomeWork.api.ShowLeaguePositionApiClient;
 import com.the23rdTeam.HomeWork.domain.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Service;
 public class LolEncryptedSummonerIdService {
     @Autowired
     private EncryptedSummonerIdApiClient encryptedSummonerIdApiClient;
+
+    @Autowired
+    private ShowLeaguePositionApiClient showLeaguePositionApiClient;
 
     public SummonerDTO getEncryptedSummonerIdApiClient(){
         SummonerDTO EncryptedSummonersInfo = encryptedSummonerIdApiClient.getEncryptedSummonerId();
