318b47f6348a5dd1c734e098c21796aae264c5d6
ImHoJeong <39ghwjd@naver.com>
2019-04-14 16:09:38 +0900

    Api 반환값이 Set형식 이므로 set 객체를 필드에 추가

--- src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
+++ src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
@@ -2,9 +2,13 @@ package com.the23rdTeam.HomeWork.service;
 
 import com.the23rdTeam.HomeWork.api.EncryptedSummonerIdApiClient;
 import com.the23rdTeam.HomeWork.api.ShowLeaguePositionApiClient;
+import com.the23rdTeam.HomeWork.domain.LeaguePositionPTO;
 import com.the23rdTeam.HomeWork.domain.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
 
 @Service
 public class LolEncryptedSummonerIdService {
@@ -13,6 +17,8 @@ public class LolEncryptedSummonerIdService {
 
     @Autowired
     private ShowLeaguePositionApiClient showLeaguePositionApiClient;
+
+    private Set<LeaguePositionPTO> leaguePositionPTOSet = new Set<LeaguePositionPTO>();
 
     public SummonerDTO getEncryptedSummonerIdApiClient(){
         SummonerDTO EncryptedSummonersInfo = encryptedSummonerIdApiClient.getEncryptedSummonerId();
