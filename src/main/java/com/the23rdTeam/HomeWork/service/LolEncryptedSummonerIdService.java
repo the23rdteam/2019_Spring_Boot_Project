b9524e83c8a02aebdbb3cfdf14ea004b2b4ecd4c
ImHoJeong <39ghwjd@naver.com>
2019-04-14 14:34:25 +0900

    api 패키지에 ShowLeaguePositionApiClient 클래스 정의 후 @Service 어노테이션 추가

--- src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
+++ src/main/java/com/the23rdTeam/HomeWork/service/LolEncryptedSummonerIdService.java
@@ -5,9 +5,6 @@ import com.the23rdTeam.HomeWork.wrapper.SummonerDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.List;
-
 @Service
 public class LolEncryptedSummonerIdService {
     @Autowired
